20bcd6371289bfc1d69626968dbf17b63b755831
Roni Ronaldo <ronironaldo64@gmail.com>
2019-04-12 14:02:36 +0700

    Extracting a Reusable Http Service

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpService";
 import "./App.css";
-
-axios.interceptors.response.use(null, error => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-
-  if (!expectedError) {
-    console.log("Logging the error", error);
-
-    alert("An unexpected error occurred");
-  }
-
-  return Promise.reject(error);
-});
 
 const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 class App extends Component {
@@ -24,13 +9,13 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const { data: posts } = await axios.get("s" + apiEndpoint);
+    const { data: posts } = await http.get("s" + apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(apiEndpoint, obj);
 
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
@@ -38,7 +23,7 @@ class App extends Component {
 
   handleUpdate = async post => {
     post.title = "UPDATED";
-    const { data } = await axios.put(apiEndpoint + "/" + post.id, post);
+    const { data } = await http.put(apiEndpoint + "/" + post.id, post);
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -53,7 +38,7 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await axios.delete("s" + apiEndpoint + "/" + post.id);
+      await http.delete("s" + apiEndpoint + "/" + post.id);
       throw new Error("");
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
